88257ea6579a5bb2b0f13dcbb0eb3780df26249b
Renan S. Parise <43964756+Renan-Parise@users.noreply.github.com>
2023-11-27 15:39:44 -0300

    Update hamiltoniano-grafico.py

--- hamiltoniano-grafico.py
+++ hamiltoniano-grafico.py
@@ -2,74 +2,75 @@ import itertools
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def find_hamiltonian_circuits(graph):
+def encontrar_circuitos_hamiltonianos(grafo):
 
-	def is_valid(v, pos, path):
-		if graph[path[pos - 1]][v] == 0:
-			return False
-		if v in path:
-			return False
-		return True
+    def e_valido(v, pos, caminho):
+        if grafo[caminho[pos - 1]][v] == 0:
+            return False
+        if v in caminho:
+            return False
+        return True
 
-	def hamiltonian_util(path, pos):
-		if pos == len(graph):
-			return True
+    def util_hamiltoniana(caminho, pos):
+        if pos == len(grafo):
+            return True
 
-		for v in range(len(graph)):
-			if is_valid(v, pos, path):
-				path[pos] = v
+        for v in range(len(grafo)):
+            if e_valido(v, pos, caminho):
+                caminho[pos] = v
 
-				if hamiltonian_util(path, pos + 1):
-					return True
+                if util_hamiltoniana(caminho, pos + 1):
+                    return True
 
-				path[pos] = -1
+                caminho[pos] = -1
 
-		return False
+        return False
 
-	def calculate_path_cost(path):
-		cost = 0
-		for i in range(len(path) - 1):
-			cost += graph[path[i]][path[i + 1]]
-		return cost
+    def calcular_custo_caminho(caminho):
+        custo = 0
+        for i in range(len(caminho) - 1):
+            custo += grafo[caminho[i]][caminho[i + 1]]
+        return custo
 
-	n = len(graph)
-	path = [-1] * n
-	path[0] = 0
+    n = len(grafo)
+    caminho = [-1] * n
+    caminho[0] = 0
 
-	if not hamiltonian_util(path, 1):
-		print("Não existe um circuito hamiltoniano possível.")
-		return
+    if not util_hamiltoniana(caminho, 1):
+        print("Não existe um circuito hamiltoniano possível.")
+        return
 
-	G = nx.Graph()
-	G.add_nodes_from(range(n))
+    G = nx.Graph()
+    G.add_nodes_from(range(n))
 
-	for i in range(n):
-		for j in range(i + 1, n):
-			if graph[i][j] != 0:
-				G.add_edge(i, j, weight=graph[i][j])
+    for i in range(n):
+        for j in range(i + 1, n):
+            if grafo[i][j] != 0:
+                G.add_edge(i, j, weight=grafo[i][j])
 
-	pos = nx.spring_layout(G)
-	labels = {i: f'{chr(ord("A") + i)}' for i in G.nodes}
+    pos = nx.spring_layout(G)
+    labels = {i: f'{chr(ord("A") + i)}' for i in G.nodes}
 
-	nx.draw(G, pos, with_labels=True, labels=labels)
-	edge_labels = nx.get_edge_attributes(G, 'weight')
-	nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+    nx.draw(G, pos, with_labels=True, labels=labels)
+    edge_labels = nx.get_edge_attributes(G, 'weight')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
 
-	plt.show()
+    plt.show()
 
-	paths = []
-	for perm in itertools.permutations(range(1, n)):
-		perm = (0, ) + perm + (0, )
-		paths.append(perm)
+    caminhos = []
+    for perm in itertools.permutations(range(1, n)):
+        perm = (0, ) + perm + (0, )
+        caminhos.append(perm)
 
-	paths.sort(key=lambda x: calculate_path_cost(x))
+    caminhos.sort(key=lambda x: calcular_custo_caminho(x))
 
-	for path in paths:
-		cost = calculate_path_cost(path)
-		city_names = ''.join(chr(ord('A') + i) for i in path)
-		print(f"Caminho {city_names}: ({cost})")
+    for caminho in caminhos:
+        custo = calcular_custo_caminho(caminho)
+        nomes_cidades = ''.join(chr(ord('A') + i) for i in caminho)
+        print(f"Caminho {nomes_cidades}: ({custo})")
 
 
-graph = [[0, 4, 2, 1], [4, 0, 13, 9], [2, 13, 0, 8], [1, 9, 8, 0]]
-
-find_hamiltonian_circuits(graph)
+encontrar_circuitos_hamiltonianos([[0, 4, 2, 1],
+                                   [4, 0, 13, 9],
+                                   [2, 13, 0, 8],
+                                   [1, 9, 8, 0]])
